a7186351fdd91f36b482ba8ebd93f12fce6e388d
tuxudo <tuxudo@users.noreply.github.com>
2023-02-27 15:20:15 -0500

    Clean up whitespace

--- caching_model.php
+++ caching_model.php
@@ -69,14 +69,14 @@ class Caching_model extends \Model
     }
 
     // ------------------------------------------------------------------------
-    
+
     /**
      * Get reachability IP address for widget
      *
      **/
     public function get_reachable_cache_name()
     {
-        $sql = "SELECT COUNT(CASE WHEN reachability <> '' AND reachability IS NOT NULL THEN 1 END) AS count, reachability 
+        $sql = "SELECT COUNT(CASE WHEN reachability <> '' AND reachability IS NOT NULL THEN 1 END) AS count, reachability
                 FROM caching
                 LEFT JOIN reportdata USING (serial_number)
                 ".get_machine_group_filter()."
@@ -89,15 +89,15 @@ class Caching_model extends \Model
                 $out[] = $obj;
             }
         }
-        
+
         return $out;
     }
-    
+
     /**
      * Process data sent by postflight
      *
      * @param string data
-	 * @author tuxudo
+     * @author tuxudo
      **/
     public function process($data)
     {
@@ -106,7 +106,7 @@ class Caching_model extends \Model
             // Throw error if no data
             print_r("Error Processing Caching Module Request: No data found");
         } else if (substr( $data, 0, 26 ) != '[{"name":"status","result"' ) { // Else if old style text, process with old text based handler
-            
+
             // Delete previous entries
             $this->deleteWhere('serial_number=?', $this->serial_number);
 
@@ -114,47 +114,47 @@ class Caching_model extends \Model
             $i=1;
             $c=21;
             $pastEight = (time()-691200);
-            
+
             // Parse data
             foreach(explode("\n", $data) as $line) {
                 $cache_line = explode("|", $line);
 
                 if (! empty($line)) {
-                      $cache_array[(str_replace(".", "", $cache_line[3]))] = $cache_line[4]; 
+                      $cache_array[(str_replace(".", "", $cache_line[3]))] = $cache_line[4];
                       $i++;
-                    
+
                     if ( $i == 22 ) {
                         // Check if data is from the past 8 one days
                         if ($cache_line[1] > $pastEight){
                             $dt = new DateTime("@$cache_line[1]");
                             $cache_array['collectiondate'] = ($dt->format('Y-m-d H:i:s'));
                             $dt = new DateTime("@$cache_line[2]");
-                            $cache_array['expirationdate'] = ($dt->format('Y-m-d H:i:s')); 
-                            $cache_array['collectiondateepoch'] = $cache_line[1]; 
+                            $cache_array['expirationdate'] = ($dt->format('Y-m-d H:i:s'));
+                            $cache_array['collectiondateepoch'] = $cache_line[1];
 
                             foreach($cache_array as $cache_item => $item) {
-                                $this->$cache_item = $cache_array[$cache_item]; 
+                                $this->$cache_item = $cache_array[$cache_item];
                             }
 
-                            // Save the entry                            
+                            // Save the entry
                             $this->id = '';
-                            $this->create(); 
+                            $this->create();
                             $i=1;
                         } else {
                             // If data is older than 31 days, skip it
-                            $i=1; 
+                            $i=1;
                         }
                     }
                 }
             } // End foreach explode lines
         } else { // Process data with new, fancy pants JSON handler
-            
+
             // Delete previous entries, bye bye data
             $this->deleteWhere('serial_number=?', $this->serial_number);
                     
             // Process JSON into PHP object thingy
             $cachingjson = json_decode($data, true);
-            
+
             // Translate caching object strings to db fields
             $translate = array(
                 'Activated' => 'activated',
@@ -186,7 +186,7 @@ class Caching_model extends \Model
                 'TotalBytesStoredFromParents' => 'totalbytesstoredfromparents',
                 'TotalBytesStoredFromPeers' => 'totalbytesstoredfrompeers'
             );
-            
+
             $cachedetailstranslate = array (
                 'Apple TV Software' => 'appletvsoftware',
                 'Mac Software' => 'macsoftware',
@@ -198,42 +198,42 @@ class Caching_model extends \Model
                 'Music' => 'musicdata',
                 'Other' => 'otherdata',
             );
-            
+
             $booleans = array('activated','active','registrationstatus','restrictedmedia');
-        
+
             $nestedarrays = array('cachedetails');
-                        
+
             // Traverse the caching object with translations
             foreach ($translate as $search => $field) { 
 
                 if (! is_array($cachingjson[0]["result"])){
                     // If not an array, null the field
                     $this->$field = '';
-                    
+
                 } else if (! array_key_exists($search, $cachingjson[0]["result"])){
                     // Skip keys that may not exist and null the value
                     $this->$field = '';
-                                
+
                     // Format booleans before processing
                 } else if (in_array($field, $booleans) && ($cachingjson[0]["result"][$search] == "true" || $cachingjson[0]["result"][$search] == "1")) {
                     // Send a 1 to the db
                     $this->$field = '1';
-                    
+
                 } else if (in_array($field, $booleans) && ($cachingjson[0]["result"][$search] == "false" || $cachingjson[0]["result"][$search] == "0")) {
                     // Send a 0 to the db
                     $this->$field = '0';
 
-                } else if (! empty($cachingjson[0]["result"][$search]) && ! is_array($cachingjson[0]["result"][$search])) { 
+                } else if (! empty($cachingjson[0]["result"][$search]) && ! is_array($cachingjson[0]["result"][$search])) {
                     // If key is not empty, save it to the object
                     $this->$field = $cachingjson[0]["result"][$search];
-                    
+
                 } else if (is_array($cachingjson[0]["result"][$search]) && ! in_array($field, $nestedarrays) && ! empty($cachingjson[0]["result"][$search])){
                     // If is an array and not a nested array, and is not empty, condense it to a string and save it
-                    $this->$field = implode(", ", $cachingjson[0]["result"][$search]);                    
-                    
-                } else if ($search == "CacheDetails" && ! empty($cachingjson[0]["result"][$search])){                    
+                    $this->$field = implode(", ", $cachingjson[0]["result"][$search]);
+
+                } else if ($search == "CacheDetails" && ! empty($cachingjson[0]["result"][$search])){
                     // Fill out the caching details values from the CacheDetails array
-                    
+
                     foreach ($cachedetailstranslate as $detailssearch => $detailsfield){
                         if (! empty($cachingjson[0]["result"][$search][$detailssearch])) {
                             // If detail search isn't empty, save value
@@ -243,24 +243,24 @@ class Caching_model extends \Model
                             $this->$detailsfield = "0";
                         }
                     }
-                    
+
                 } else if ($cachingjson[0]["result"][$search] == "0" && ! is_array($cachingjson[0]["result"][$search])){
-                    // Set the value to 0 if it's 0                        
+                    // Set the value to 0 if it's 0
                     $this->$field = "0";
                 } else {  
                     // Else, null the value
                     $this->$field = '';
                 }
             }
-            
-            if($cachingjson[0]["result"]['exact_metrics']){
+
+            if(array_key_exists(0, $cachingjson) && $cachingjson[0]["result"]['exact_metrics']){
                 $this->expirationdate = -1;
             }
-            
+
             // Save it, like that cake you just dropped onto the floor after pulling it out of the oven. Yea, I saw that
             $this->id = '';
-            $this->create(); 
-            
+            $this->create();
+
             // Process exact_metrics, if it exists
             if($cachingjson[0]["result"]['exact_metrics']){
                 $pastThirtyone = (time()-2678400);
@@ -270,12 +270,12 @@ class Caching_model extends \Model
                 foreach($exact_array as $metric){
                     // Make sure entry isn't empty
                     if (! empty($metric)) {
-                        // Split entry 
+                        // Split entry
                         $metric_array = explode(",",$metric);
-                        
+
                         // Format collection date epoch
                         $metric_array[0] = (round(explode(".",$metric_array[0])[0])+978307200);
-                        
+
                         // Only save data if it is less than 31 days old
                         if ($metric_array[0] > $pastThirtyone){
                             // Assign data to columns from metric_array
