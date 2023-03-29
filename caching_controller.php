112edaa88ea854f275e166589cd550131c694dd8
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:42:24 -0400

    Update caching_controller.php

--- caching_controller.php
+++ caching_controller.php
@@ -53,11 +53,14 @@ class Caching_controller extends Module_controller
      **/
     public function get_client_tab_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
         if (! $this->authorized()) {
             $obj->view('json', array('msg' => 'Not authorized'));
             return;
-    }
+        }
 
         $queryobj = new Caching_model();
         
